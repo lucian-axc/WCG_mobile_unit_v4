8f4a3e57bb54e9702515fe19fe140caeca53f622
Lucian - Alex Coseru <lucian.alexcoseru@yahoo.com>
2024-05-30 18:43:23 +0200

    Specified port.

--- app.py
+++ app.py
@@ -5,3 +5,6 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8001)
